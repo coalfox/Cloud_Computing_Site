3edc68004774d9d23302bd81035c5e0c4c4c80ca
Inabiaf <cole@Coles-MacBook-Pro-8.local>
2018-12-13 04:12:57 -0500

    images!
    yeah they’re over 100k

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,8 +1,9 @@
 <nav>
 		<a class="addressButton" href="mailto:foxgc@mymail.vcu.edu">VCU Email Me @foxgc</a>
 		
-		<a href="http://georgecolefox.com">&copy;George Fox</a>
-
+		<a href="https://github.com/coalfox/Cloud_Computing_Site">See this on Github</a>
+		
+		<a href="http://georgecolefox.com">&copy; George Fox 2018</a>
 </nav>
 
 </body>
--- index.php
+++ index.php
@@ -21,5 +21,16 @@
 <p>Now, cloud computing is the backbone of much of the front-end of the internet, including social media, online stores and catalogs, news, forums, blogs, and even advertisement. As moving to the cloud has become almost undisputedly cheaper than setting up infrastructure and hosting on site, the primary basis for drawbacks and limitations stems from control over the data. While data security and redundancies are improved from an on site solution, questions and disputes still arise about data privacy, especially over national boundaries. Specifically, in recent events, the EU’s General Data Protection Regulation (GDPR) has had major effects on cloud services and international data storage worldwide. Part of this law called for every end user to be required to provide explicit, expressed consent to allow their usage or analytical data to be shared with any third party, which caused immediate changes from well known businesses who utilize cloud services like Google, Amazon, Twitter, Facebook, and many more. Whenever any country changes international, or even local policies regarding telecommunications, privacy, or data sharing laws, they can have drastic effects on cloud services hosted within their borders, or their end user citizens ability to access data elsewhere. Decisions limiting the availability of data made by countries large enough may cover enough users that even a foreign hosted cloud service may have to rethink its data storage methods to be compliant with new standards. For these reasons, cloud computing is still a rapidly changing and growing industry.</p>
 </div>
 </article>
+<article>
+<div class=”media”>
+<img src="img/amazon_aws_logo.png" alt="Amazon AWS Logo">
+</div>
+<div class=”media”>
+<img src="img/rackspace_logo.png" alt="Rackspace Logo">
+</div>
+<div class=”media”>
+<img src="img/Microsoft_Azure_Logo.svg" alt="Microsoft Azure Logo">
+</div>
+</article>
 </main>
 <?php include("./_includes/footer.php"); //inserts page footer ?>
